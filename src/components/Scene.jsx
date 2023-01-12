b49e957d5380c73c76fd408d6d2fa4d6b03b13b4
Cody Alan Stubbs <castubbs3@gmail.com>
2023-01-12 15:16:05 -0500

    Remove junk and add image component

---
+++ src/components/Scene.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SceneImg from "../assets/images/scene-winter.jpeg";
+
+function Scene() {
+  return (
+    <img
+      width="600px"
+      src={SceneImg}
+      alt="A Where's Waldo scene."
+      className="App"
+    />
+  );
+}
+
+export default Scene;
