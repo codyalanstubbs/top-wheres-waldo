d87b5bb89582fedd798ed4164dfdfce870807153
Cody Alan Stubbs <castubbs3@gmail.com>
2023-01-13 10:28:37 -0500

    Create Scene to display image

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import SceneImg from "../assets/images/scene-winter.jpeg";
+import "../assets/css/Scene.css";
 
 function Scene() {
+  const handleClick = (e) => {
+    const img = document.querySelector("img");
+    const x = e.pageX - img.offsetLeft;
+    const y = e.pageY - img.offsetTop;
+    console.log("x: ", x);
+    console.log("y: ", y);
+  };
+
   return (
-    <img
-      width="600px"
-      src={SceneImg}
-      alt="A Where's Waldo scene."
-      className="App"
-    />
+    <button className="scene-btn" type="button" onClick={handleClick}>
+      <img
+        width="600px"
+        src={SceneImg}
+        alt="A Where's Waldo scene."
+        className="App"
+      />
+    </button>
   );
 }
 
