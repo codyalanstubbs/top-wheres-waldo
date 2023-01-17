cdd136a64631c3a23c5cfb3f21fe2c8c9be894eb
Cody Alan Stubbs <castubbs3@gmail.com>
2023-01-17 16:01:22 -0500

    Add a leaderboard feature for after user submit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,16 @@ import React, { useCallback, useState } from "react";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import uniqid from "uniqid";
 import { initializeApp } from "firebase/app";
-import { getFirestore, doc, setDoc } from "firebase/firestore";
+import {
+  query,
+  orderBy,
+  getFirestore,
+  doc,
+  setDoc,
+  collection,
+  limit,
+  getDocs,
+} from "firebase/firestore";
 import Character from "./components/Character";
 import Timer from "./components/Timer";
 import Scene from "./components/Scene";
@@ -14,6 +23,8 @@ import Wizard from "./assets/images/wizard_400x400.png";
 function App() {
   const [startScreen, setStartScreen] = useState(true);
   const [endScreen, setEndScreen] = useState(false);
+  const [leaderboardDisplay, setLeaderboardDisplay] = useState(false);
+  const [leaderboardData, setLeaderboardData] = useState([]);
 
   const [charactersFound, setCharactersFound] = useState([
     { name: "waldo", found: false, src: Waldo },
@@ -116,6 +127,53 @@ function App() {
       minutes,
       seconds,
     });
+    // Transition from submit data screen to leaderboard scene
+    setEndScreen(false);
+    setLeaderboardDisplay(true);
+  }
+
+  function retrieveLeaderboard() {
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+      apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+      authDomain: "top-wheres-waldo-b4094.firebaseapp.com",
+      projectId: "top-wheres-waldo-b4094",
+      storageBucket: "top-wheres-waldo-b4094.appspot.com",
+      messagingSenderId: "811092831655",
+      appId: "1:811092831655:web:be49ee2a65374322fcda82",
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+
+    // Initialize Cloud Firestore and get a reference to the service
+    const db = getFirestore(app);
+    const usersRef = collection(db, "users");
+
+    // Build query for getting top 20 fastest players
+    const q = query(
+      usersRef,
+      orderBy("hours", "desc"),
+      orderBy("minutes", "desc"),
+      orderBy("seconds", "desc"),
+      limit(20)
+    );
+
+    // Build a leaderboard data array for setting to the state
+    const boardData = (async () => {
+      const querySnapshot = await getDocs(q);
+      const data = [];
+      let rank = 1;
+      querySnapshot.forEach((u) => {
+        const user = u.data();
+        data.push({ rank, user });
+        rank += 1;
+      });
+      return data;
+    })();
+
+    // Once the data is retrieved, then set leadboardData state
+    boardData.then((data) => setLeaderboardData(data));
   }
 
   if (startScreen) {
@@ -142,6 +200,38 @@ function App() {
         </label>
         <button type="button" onClick={handleSubmitScore}>
           Submit Score
+        </button>
+      </div>
+    );
+  }
+
+  if (leaderboardDisplay) {
+    retrieveLeaderboard();
+    return (
+      <div className="App">
+        <h1 className="instructions">You&apos;re score has been submitted!</h1>
+        <table>
+          <tr>
+            <th>Rank</th>
+            <th>Username</th>
+            <th>Hours</th>
+            <th>Minutes</th>
+            <th>Seconds</th>
+          </tr>
+          {leaderboardData.map((row) => {
+            return (
+              <tr>
+                <td>{row.rank}</td>
+                <td>{row.user.name}</td>
+                <td>{row.user.hours}</td>
+                <td>{row.user.minutes}</td>
+                <td>{row.user.seconds}</td>
+              </tr>
+            );
+          })}
+        </table>
+        <button type="button" onClick={handleStartClick}>
+          Play Again?
         </button>
       </div>
     );
